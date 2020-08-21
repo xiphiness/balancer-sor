7e94b8c5caa0abf6434725ab38ea3944a910b28a
JG <johngrantuk@googlemail.com>
2020-08-21 11:16:01 +0100

    Tidied multicall test.

--- test/testScripts/multicallTest.ts
+++ test/testScripts/multicallTest.ts
@@ -1,22 +1,42 @@
 require('dotenv').config();
-import fetch from 'isomorphic-fetch';
 const sor = require('../../src');
-const BigNumber = require('bignumber.js');
-const { ethers, utils } = require('ethers');
-import { Pool } from '../../src/direct/types';
-import { BONE, calcOutGivenIn, calcInGivenOut } from '../../src/bmath';
+const { ethers } = require('ethers');
 import { JsonRpcProvider } from 'ethers/providers';
 import _ from 'lodash'; // Import the entire lodash library
 
-async function run() {
-    const multicall = '0xeefBa1e63905eF1D7ACbA5a8513c70307C1cE441';
+const multicall = '0xeefBa1e63905eF1D7ACbA5a8513c70307C1cE441';
+
+async function runSingle() {
     let provider = new JsonRpcProvider(
         `https://mainnet.infura.io/v3/${process.env.INFURA}`
     );
-    const WETH = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'; // WETH
-    const DAI = '0x6B175474E89094C44Da98b954EedeAC495271d0F'; // DAI
-    const LINK = '0x514910771af9ca656af840dff83e8264ecf986ca';
-    const IMBTC = '0x3212b29e33587a00fb1c83346f5dbfa69a458923';
+
+    let allPools = await sor.getAllPublicSwapPools();
+
+    console.log(allPools.pools.length);
+    try {
+        console.time('multi');
+        let allPoolsOnChain = await sor.getAllPoolDataOnChain(
+            allPools,
+            multicall,
+            provider
+        );
+        console.timeEnd('multi');
+
+        console.log(`First Pool:`);
+        console.log(Object.values(allPoolsOnChain.pools)[0]);
+    } catch (error) {
+        console.log(`ERROR ${error.message}`);
+    }
+
+    return;
+}
+
+// Increases number of multicalls to test limit.
+async function runLoopTest() {
+    let provider = new JsonRpcProvider(
+        `https://mainnet.infura.io/v3/${process.env.INFURA}`
+    );
 
     let allPools = await sor.getAllPublicSwapPools();
     let i = 0;
@@ -45,4 +65,4 @@ async function run() {
     return;
 }
 
-run();
+runSingle();
